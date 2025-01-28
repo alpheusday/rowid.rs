3f905da4deb65a4792b633969d15501abe3cdfb5
Alpheus <26622568+alpheustangs@users.noreply.github.com>
2025-01-28 13:24:31 +0800

    Updates for rand

--- package/src/functions/get_randomness.rs
+++ package/src/functions/get_randomness.rs
@@ -11,10 +11,10 @@ pub fn _get_randomness<CharList: AsRef<str>>(
     let char_list: Vec<char> = opts.char_list.as_ref().chars().collect();
     let length: usize = opts.randomness_length;
     let mut randomness: Vec<char> = Vec::with_capacity(length);
-    let mut rng: ThreadRng = rand::thread_rng();
+    let mut rng: ThreadRng = rand::rng();
 
     for _ in 0..length {
-        let index: usize = rng.gen_range(0..char_list.len());
+        let index: usize = rng.random_range(0..char_list.len());
         randomness.push(char_list[index]);
     }
 
